4eb558c385724fb149c2808db42668e2295cf0a8
witk44 <69919362+witk44@users.noreply.github.com>
2023-07-06 14:18:20 -0400

    signup page done

--- Login.py
+++ Login.py
@@ -3,18 +3,24 @@ from tkinter import *
 
 def login():
     master = Tk()
-    Label(master, text='First Name').grid(row=0)
-    Label(master, text='Last Name').grid(row=1)
+    Label(master, text='Username').grid(row=0)
+    Label(master, text='Password').grid(row=1)
     first_name = StringVar()
     last_name = StringVar()
-    first_name_entry = Entry(master, textvariable = first_name)
+    first_name_entry = Entry(master, textvariable=first_name)
     last_name_entry = Entry(master, textvariable=last_name)
     
     first_name_entry.grid(row=0, column=1)
     last_name_entry.grid(row=1, column=1)
-    mainloop()
-    fname = first_name.get()
-    lname = last_name.get()
-    print("First Name: " + fname)
-    print("Last Name: " + lname)
-    return True
+
+    def get_names():  # Use nonlocal to update the global variables
+        fname = first_name_entry.get()
+        lname = last_name_entry.get()
+        print("Username: ", fname)
+        print("Password: ", lname)
+        master.destroy()
+
+    submit_button = Button(master, text="Submit", command=get_names)
+    submit_button.grid(row=2, columnspan=2)
+
+    master.mainloop()
--- main.py
+++ main.py
@@ -9,18 +9,29 @@ def create_page():
     set_dimensions(window)
     window.geometry(f"{screen_width}x{screen_height}")
     # Set the background color
-    window.configure(bg="lightblue")  # Set the color name or use hex code
+    window.configure(bg="#94BFA7")  # Set the color name or use hex code
+    # Create a frame for centering the text box
+    frame = Frame(window)
+    frame.pack(pady=20)
 
+    # Create the text box with default text
+    text_entry = Entry(frame, font=("Arial", 24), bg="#E0B7B7", width=23)
+    text_entry.insert(0, "Welcome to SayYes Connect")
+    text_entry.pack()
+
+    # Position the frame at the center top
+    frame.place(relx=0.5, rely=0, anchor=N)
     # Create the login button
-    login_button = Button(window, text="Login", command=login, width = int(screen_width//100), height=int(screen_height//100))
+    login_button = Button(window, text="Login", command=login, width = int(screen_width//100), height=int(screen_height//100),background="#F7A1C4")
     login_button.pack(pady=20,expand=False, side=TOP)
     login_button.place(x=int((screen_width//2)-(screen_width//200)-100),y=int(screen_height//2))
     # Create the sign-up button
-    signup_button = Button(window, text="Sign Up", command=signUp, width = int(screen_width//100), height=int(screen_height//100))
+    signup_button = Button(window, text="Sign Up", command=signUp, width = int(screen_width//100), height=int(screen_height//100),background="#F7A1C4")
     signup_button.pack(pady=20,expand=False, side=TOP)
     signup_button.place(x=int((screen_width//2)+(screen_width//200)),y=int(screen_height//2) )
     # Run the Tkinter event loop
     window.mainloop()
+
 
 def set_dimensions(screen):
     global screen_height
