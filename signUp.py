378a548ce00ab0350f119040eecefdcdacb5fc89
witk44 <69919362+witk44@users.noreply.github.com>
2023-07-06 11:05:57 -0400

    store first and last name

--- signUp.py
+++ signUp.py
@@ -5,11 +5,17 @@ def display():
     master = Tk()
     Label(master, text='First Name').grid(row=0)
     Label(master, text='Last Name').grid(row=1)
-    e1 = Entry(master)
-    e2 = Entry(master)
-    e1.grid(row=0, column=1)
-    e2.grid(row=1, column=1)
+    first_name = StringVar()
+    last_name = StringVar()
+    first_name_entry = Entry(master, textvariable = first_name)
+    last_name_entry = Entry(master, textvariable=last_name)
+    
+    first_name_entry.grid(row=0, column=1)
+    last_name_entry.grid(row=1, column=1)
     mainloop()
-
+    fname = first_name.get()
+    lname = last_name.get()
+    print("First Name: " + fname)
+    print("Last Name: " + lname)
 
 display()
