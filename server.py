3d66f505ff950563adf9eec323c41741878c8494
t-bex09 <rlramhap@buffalo.edu>
2023-07-06 11:21:50 -0400

    maybe ?

--- server.py
+++ server.py
@@ -2,3 +2,18 @@ import sys
 import socketserver
 import os
 
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        received_data = self.request.recv(2048)
+        sys.stdout.flush()
+        sys.stderr.flush()
+
+        decodeDataIndex = received_data.find("\r\n\r\n".encode())
+        decodedData = received_data[:decodeDataIndex].decode().split(" ")
+        reqType = decodedData[1]
+
+        if str(reqType) == "/Login":
+            self.request.sendall("HTTP/1.1 200 OK\r\nContent-Length: 18\r\nContent-Type: text/plain; charset=utf-8\r\n\r\nHello World! login".encode())
+        elif str(reqType) == "/signUp":
+            self.request.sendall("HTTP/1.1 200 OK\r\nContent-Length: 19\r\nContent-Type: text/plain; charset=utf-8\r\n\r\nHello World! signUp".encode())
+        
