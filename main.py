b9452223f7158c4a836c68ca558ebf960fdcfb9a
witk44 <69919362+witk44@users.noreply.github.com>
2023-07-06 13:17:35 -0400

    login and signup buttons

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
 from tkinter import *
 from Login import *
 from signUp import *
-global screen_height
-global screen_width
+
 
 def create_page():
     # Create the Tkinter window
     window = Tk(className="SayYes Connect")
-
-    # Set the screen size
-    window.geometry("900x600")  # Set the width and height as desired
-
+    set_dimensions(window)
+    window.geometry(f"{screen_width}x{screen_height}")
     # Set the background color
     window.configure(bg="lightblue")  # Set the color name or use hex code
 
     # Create the login button
-    login_button = Button(window, text="Login", command=login, width=10,height=5)
+    login_button = Button(window, text="Login", command=login, width = int(screen_width//100), height=int(screen_height//100))
     login_button.pack(pady=20,expand=False, side=TOP)
-    login_button.place(x=350,y=400)
+    login_button.place(x=int((screen_width//2)-(screen_width//200)-100),y=int(screen_height//2))
     # Create the sign-up button
-    signup_button = Button(window, text="Sign Up", command=signUp, width=10,height=5)
+    signup_button = Button(window, text="Sign Up", command=signUp, width = int(screen_width//100), height=int(screen_height//100))
     signup_button.pack(pady=20,expand=False, side=TOP)
-    signup_button.place(x=450,y=400 )
+    signup_button.place(x=int((screen_width//2)+(screen_width//200)),y=int(screen_height//2) )
     # Run the Tkinter event loop
     window.mainloop()
 
 def set_dimensions(screen):
-    return True
+    global screen_height
+    global screen_width
+    screen_width = screen.winfo_screenwidth()
+    screen_height = screen.winfo_screenheight()
 
 if __name__ == "__main__":
     create_page()
